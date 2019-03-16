ca9406602978c12b4a46a049ac2bdcd2c6a76202
David Peng <davidduzyn@outlook.com>
2019-03-16 10:07:09 +0800

    Added online templates.

--- test.sh
+++ test.sh
@@ -4,13 +4,16 @@ rm examples/*
 # Download css stylesheets
 aria2c -d css --allow-overwrite https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css
 aria2c -d css --allow-overwrite https://cdnjs.cloudflare.com/ajax/libs/marx/3.0.7/marx.css
-aria2c -d css --allow-overwrite https://cdnjs.cloudflare.com/ajax/libs/tufte-css/1.4/tufte.css
+aria2c -d css --allow-overwrite https://cdn.staticaly.com/gh/edwardtufte/tufte-css/v1.4/tufte.css
 aria2c -d css --allow-overwrite https://cdn.staticaly.com/gh/edwardtufte/tufte-css/v1.4/latex.css
 
 # Generate HTML templates
-pp -D TEMPLATE=github pp/template.pp.html > templates/github.html
-pp -D TEMPLATE=marx   pp/template.pp.html > templates/marx.html
-pp -D TEMPLATE=tufte  pp/template.pp.html > templates/tufte.html
+pp -D TEMPLATE=github         pp/template.pp.html > templates/github.html
+pp -D TEMPLATE=marx           pp/template.pp.html > templates/marx.html
+pp -D TEMPLATE=tufte          pp/template.pp.html > templates/tufte.html
+pp -D TEMPLATE=github-offline pp/template.pp.html > templates/github-offline.html
+pp -D TEMPLATE=marx-offline   pp/template.pp.html > templates/marx-offline.html
+pp -D TEMPLATE=tufte-offline  pp/template.pp.html > templates/tufte-offline.html
 
 # Update template tufte-handout.tex
 aria2c -d templates --allow-overwrite https://cdn.staticaly.com/gh/rstudio/tufte/master/inst/rmarkdown/templates/tufte_handout/resources/tufte-handout.tex
@@ -23,11 +26,11 @@ aria2c -o examples/MANUAL.md --allow-overwrite https://cdn.staticaly.com/gh/jgm/
 
 # Github
 pandoc examples/MANUAL.md -o examples/MANUAL-github.html -f markdown -t html5 -N --toc --template=github.html
-pandoc examples/MANUAL.md -o examples/MANUAL-github.pdf -f markdown -t html -N --toc --template=github.html -V header-html=header.html -V footer-html=footer.html
+pandoc examples/MANUAL.md -o examples/MANUAL-github.pdf -f markdown -t html -N --toc --template=github-offline.html -V header-html=header.html -V footer-html=footer.html
 
 # Marx
 pandoc examples/MANUAL.md -o examples/MANUAL-marx.html -f markdown -t html5 -N --toc --template=marx.html
-pandoc examples/MANUAL.md -o examples/MANUAL-marx.pdf -f markdown -t html -N --toc --template=marx.html -V header-html=header.html -V footer-html=footer.html
+pandoc examples/MANUAL.md -o examples/MANUAL-marx.pdf -f markdown -t html -N --toc --template=marx-offline.html -V header-html=header.html -V footer-html=footer.html
 
 # Tufte Handout
 # pandoc examples/MANUAL.md -o examples/MANUAL-tufte-handout.pdf -f markdown --pdf-engine=lxelatex -N --toc --template=tufte-handout.tex
