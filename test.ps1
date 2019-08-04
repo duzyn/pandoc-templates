30854772a1287c100512f08fe337aad3e58b1b07
David Peng <davidduzyn@outlook.com>
2019-08-04 11:44:20 +0800

    add ps1

---
+++ test.ps1
@@ -0,0 +1,32 @@
+Set-Location examples
+
+Remove-Item *.*
+
+# Update Pandoc MANUAL.txt
+aria2c -o MANUAL.md --allow-overwrite https://cdn.jsdelivr.net/gh/jgm/pandoc/MANUAL.txt
+
+# Github
+pandoc MANUAL.md -o MANUAL-github.html -f markdown -t html5 -N --toc --template=github.html -V toctitle="Table of Contents" -V lang=en-US
+
+# Marx
+pandoc MANUAL.md -o MANUAL-marx.html -f markdown -t html5 -N --toc --template=marx.html -V toctitle="Table of Contents" -V lang=en-US
+
+# wkhtmltopdf
+pandoc MANUAL.md -o MANUAL-wkhtmltopdf.pdf -f markdown -t html5 -N --toc --template=wkhtmltopdf.html -V toctitle="Table of Contents" -V lang=en-US -V footer-html=../includes/en/footer.html
+
+# PDF Article
+# pandoc MANUAL.md -o MANUAL-pdf-article.pdf -f markdown -t html --pdf-engine=prince -N --toc --template=pdf-article.html -V toctitle="Table of Contents" -V lang=en-US
+
+# PDF Report
+# pandoc MANUAL.md -o MANUAL-pdf-report.pdf -f markdown -t html --pdf-engine=prince -N --toc --template=pdf-report.html -V toctitle="Table of Contents" -V lang=en-US
+
+# PDF Book
+# pandoc MANUAL.md -o MANUAL-pdf-book.pdf -f markdown -t html --pdf-engine=prince -N --toc --template=pdf-book.html -V toctitle="Table of Contents" -V lang=en-US
+
+# Eisvogel
+# pandoc MANUAL.md -o MANUAL-eisvogel.pdf -f markdown --pdf-engine=xelatex -N --toc --template=eisvogel.tex -V titlepage=true -V toc-own-page=true
+
+# EPUB
+# pandoc MANUAL.md -o MANUAL.epub -f markdown -t epub -N --toc -V toctitle="Table of Contents" -V lang=en-US
+
+Set-Location ..
