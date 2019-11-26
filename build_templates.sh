0632e3b01dfc6ebfae5f28c799a8452f467ca39b
davis <davidduzyn@outlook.com>
2019-11-26 12:00:11 +0800

    use defaults instead of templates

--- build_templates.sh
+++
@@ -1,7 +0,0 @@
-cd templates
-
-pp -html -DTHEME=github _base.html >github.html
-pp -html -DTHEME=marx _base.html >marx.html
-pp -html -DTHEME=wkhtmltopdf _base.html >wkhtmltopdf.html
-
-cd ..
--- test.sh
+++ test.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-cd ./examples || return
 
-rm -r ./*.*
-
-# Update Pandoc MANUAL.txt
-aria2c -o MANUAL.md --allow-overwrite https://github.com/jgm/pandoc/raw/master/MANUAL.txt
+# rm -r ./examples/*.*
 
 # Github
-pandoc MANUAL.md -o MANUAL-github.html -f markdown -t html5 -N --toc --template=github.html -V toctitle="Table of Contents"
+pandoc ./examples/MANUAL.md -o ./examples/MANUAL-github.html -d html5_github.yml -d lang_en.yml -d css_pandoc.yml
 
 # Marx
-pandoc MANUAL.md -o MANUAL-marx.html -f markdown -t html5 -N --toc --template=marx.html -V toctitle="Table of Contents"
+pandoc ./examples/MANUAL.md -o ./examples/MANUAL-marx.html -d html5_marx.yml -d lang_en.yml -d css_pandoc.yml
 
 # wkhtmltopdf
-pandoc MANUAL.md -o MANUAL-wkhtmltopdf.pdf -f markdown -t html5 -N --toc --template=wkhtmltopdf.html -V toctitle="Table of Contents" -V footer-html=../includes/footer.html
+pandoc ./examples/MANUAL.md -o ./examples/MANUAL-wkhtmltopdf.pdf -d html5_github.yml -d lang_en.yml -d css_pandoc.yml -d pdf_article.yml
 
 # Eisvogel
-pandoc MANUAL.md -o MANUAL-eisvogel.pdf -f markdown --pdf-engine=xelatex -N --toc --template=eisvogel.tex -V titlepage=true -V toc-own-page=true
+pandoc ./examples/MANUAL.md -o ./examples/MANUAL-eisvogel.pdf -d latex_eisvogel.yml -d lang_en.yml
 
 # EPUB
-pandoc MANUAL.md -o MANUAL.epub -f markdown -t epub -N --toc -V toctitle="Table of Contents"
-
-cd ..
+pandoc ./examples/MANUAL.md -o ./examples/MANUAL.epub -d epub_ebook.yml -d lang_en.yml
