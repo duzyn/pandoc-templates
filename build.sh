22e128b45dfa4b4e68d68385d7d949d54f63cce8
davis <davidduzyn@outlook.com>
2019-11-29 14:17:06 +0800

    update defaults

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+rm -rf ./examples/MANUAL-*.*
+
+# Update Pandoc MANUAL.txt
+wget -P ./examples -N https://raw.githubusercontent.com/jgm/pandoc/master/MANUAL.txt
+
+# Update Eisvogel LaTeX template
+wget -P ./templates -N https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex
+
+# Update Tufte Handout LaTeX template
+wget -P ./templates -N https://raw.githubusercontent.com/rstudio/tufte/master/inst/rmarkdown/templates/tufte_handout/resources/tufte-handout.tex
+
+# Github
+pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-github.html -d html5_github.yml
+
+# Marx
+pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-marx.html -d html5_marx.yml
+
+# wkhtmltopdf
+pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-wkhtmltopdf.pdf -d pdf_wkhtmltopdf.yml
+
+# weasyprint
+# pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-weasyprint.pdf -d pdf_weasyprint.yml
+
+# Eisvogel
+# pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-eisvogel.pdf -d latex_eisvogel.yml
+
+# EPUB
+pandoc ./examples/MANUAL.txt -o ./examples/MANUAL.epub -d epub_ebook.yml
