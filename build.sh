0b8d8d1bf1c74a22babc63932cd1c71fff992b86
David Peng <davidduzyn@outlook.com>
2020-04-18 18:24:59 +0800

    modify

--- build.sh
+++ build.sh
@@ -3,28 +3,40 @@
 rm -rf ./examples/MANUAL-*.*
 
 # Update Pandoc MANUAL.txt
-wget -P ./examples -N https://cdn.jsdelivr.net/gh/jgm/pandoc/MANUAL.txt
-
-# Update Eisvogel LaTeX template
-# wget -P ./templates -N https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex
-
-# Update Tufte Handout LaTeX template
-# wget -P ./templates -N https://raw.githubusercontent.com/rstudio/tufte/master/inst/rmarkdown/templates/tufte_handout/resources/tufte-handout.tex
+wget -P ./examples \
+    -N https://cdn.jsdelivr.net/gh/jgm/pandoc/MANUAL.txt
 
 # Github
-pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-github.html -d html5_github.yml
+pandoc ./examples/MANUAL.txt \
+    -o ./examples/MANUAL-github.html \
+    -d ./defaults/html5_github.yml
 
 # Marx
-pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-marx.html -d html5_marx.yml
+pandoc ./examples/MANUAL.txt \
+    -o ./examples/MANUAL-marx.html \
+    -d ./defaults/html5_marx.yml
 
-# wkhtmltopdf
-pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-wkhtmltopdf.pdf -d pdf_wkhtmltopdf.yml
-
-# weasyprint
-pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-weasyprint.pdf -d pdf_weasyprint.yml
+# Tufte
+pandoc ./examples/MANUAL.txt \
+    -o ./examples/MANUAL-tufte.html \
+    -d ./defaults/html5_tufte.yml
 
 # Eisvogel
-pandoc ./examples/MANUAL.txt -o ./examples/MANUAL-eisvogel.pdf -d latex_eisvogel.yml
+pandoc ./examples/MANUAL.txt \
+    -o ./examples/MANUAL-eisvogel.pdf \
+    -d ./defaults/latex_eisvogel.yml
+
+# weasyprint
+pandoc ./examples/MANUAL.txt \
+    -o ./examples/MANUAL-weasyprint.pdf \
+    -d ./defaults/pdf_weasyprint.yml
+
+# wkhtmltopdf
+pandoc ./examples/MANUAL.txt \
+    -o ./examples/MANUAL-wkhtmltopdf.pdf \
+    -d ./defaults/pdf_wkhtmltopdf.yml
 
 # EPUB
-pandoc ./examples/MANUAL.txt -o ./examples/MANUAL.epub -d epub_ebook.yml
+pandoc ./examples/MANUAL.txt \
+    -o ./examples/MANUAL.epub \
+    -d ./defaults/epub_ebook.yml
