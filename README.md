634af6ae2a781129fb2f64722a9a32f14a81276a
David Peng <davidduzyn@outlook.com>
2023-07-16 20:16:44 +0800

    update

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
----
-title: Pandoc 模板
-author: David Peng
-date: 2023-07-12
-CJKmainfont: Noto Serif CJK SC
-CJKsansfont: Noto Sans CJK SC
-CJKmonofont: Noto Sans Mono CJK SC
-wkhtmltopdfCJKmainfont: 思源宋体
-wkhtmltopdfCJKsansfont: 思源黑体
-wkhtmltopdfCJKmonofont: 思源等宽
----
-
-<!-- markdownlint-disable MD025 -->
-
 # Pandoc 模板
 
 自用的一些 Pandoc 模板
@@ -20,11 +6,13 @@ wkhtmltopdfCJKmonofont: 思源等宽
 
 在 Windows 上，克隆本仓库到 `%APPDATA%\pandoc` 路径下：
 
-    git clone https://github.com/duzyn/pandoc-templates "%APPDATA%\pandoc"
+    git clone https://ghproxy.com/https://github.com/duzyn/pandoc-templates \
+    "%APPDATA%\pandoc"
 
 在 Linux 或 macOS 上，克隆本仓库到 `~/.local/share/pandoc` 路径下：
 
-    git clone https://github.com/duzyn/pandoc-templates ~/.local/share/pandoc
+    git clone https://ghproxy.com/https://github.com/duzyn/pandoc-templates \
+    ~/.local/share/pandoc
 
 ## 参考
 
--- update.sh
+++ update.sh
@@ -18,52 +18,52 @@ if [[ "$DEBUG" == "true" ]]; then
 fi
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${0}")")"
-mkdir -p "$SCRIPT_DIR/examples" "$SCRIPT_DIR/templates"
-
 cd "$SCRIPT_DIR" || exit 1
 
+mkdir -p examples templates translations
+
 # Clean
-[[ -f ./examples/pandoc-manual.md ]]          && rm ./examples/pandoc-manual.md
-[[ -f ./examples/README.md ]]                 && rm ./examples/README.md
-[[ -f ./templates/eisvogel.tex ]]             && rm ./templates/eisvogel.tex
-[[ -f ./templates/tufte-handout-origin.tex ]] && rm ./templates/tufte-handout-origin.tex
+# [[ -f examples/spec.md ]] && rm examples/spec.md
+# [[ -f examples/README.md ]] && rm examples/README.md
+# [[ -f templates/eisvogel.tex ]] && rm templates/eisvogel.tex
+# [[ -f templates/_tufte-handout-origin.tex ]] && rm templates/_tufte-handout-origin.tex
 
 # Update source
-wget -O ./examples/pandoc-manual.md          https://ghproxy.com/https://raw.githubusercontent.com/jgm/pandoc/master/MANUAL.txt
-wget -O ./templates/eisvogel.tex             https://ghproxy.com/https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex
-wget -O ./templates/tufte-handout-origin.tex https://ghproxy.com/https://raw.githubusercontent.com/rstudio/tufte/master/inst/rmarkdown/templates/tufte_handout/resources/tufte-handout.tex
-wget -O ./templates/github-markdown.css      https://ghproxy.com/https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css
+# wget -O examples/spec.md https://ghproxy.com/https://raw.githubusercontent.com/commonmark/commonmark-spec/master/spec.txt
+wget -O templates/eisvogel.tex https://ghproxy.com/https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex
+wget -O templates/_tufte-handout-origin.tex https://ghproxy.com/https://raw.githubusercontent.com/rstudio/tufte/master/inst/rmarkdown/templates/tufte_handout/resources/tufte-handout.tex
+wget -O templates/_github-markdown.css https://ghproxy.com/https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css
 
 # epub.css
-cat ./templates/base.css   >  ./epub.css
-cat ./templates/pandoc.css >> ./epub.css
+cp -f templates/_base.css epub.css
+cat templates/_pandoc.css >> epub.css
 
 # Translations
 pandoc --print-default-data-file=translations/zh-Hans.yaml > translations/zh-CN.yaml
 pandoc --print-default-data-file=translations/zh-Hant.yaml > translations/zh-TW.yaml
 
 # Test
-pandoc                                     -o ./examples/pandoc-manual.epub              ./examples/pandoc-manual.md --toc
-pandoc -d defaults/docx.yml                -o ./examples/pandoc-manual.docx              ./examples/pandoc-manual.md --toc 
-pandoc -d defaults/html5_github.yml        -o ./examples/pandoc-manual-github.html       ./examples/pandoc-manual.md --toc  
-pandoc -d defaults/latex.yml               -o ./examples/pandoc-manual.tex               ./examples/pandoc-manual.md --toc 
-pandoc -d defaults/pdf_eisvogel.yml        -o ./examples/pandoc-manual-eisvogel.pdf      ./examples/pandoc-manual.md --toc -M titlepage=true
-pandoc -d defaults/pdf_pagedjs-cli.yml     -o ./examples/pandoc-manual-pagedjs-cli.pdf   ./examples/pandoc-manual.md --toc 
-pandoc -d defaults/pdf_wkhtmltopdf.yml     -o ./examples/pandoc-manual-wkhtmltopdf.pdf   ./examples/pandoc-manual.md --toc 
-pandoc -d defaults/pdf_xelatex.yml         -o ./examples/pandoc-manual-xelatex.pdf       ./examples/pandoc-manual.md --toc 
-# pandoc -d defaults/pdf_typst.yml           -o ./examples/pandoc-manual-typst.pdf         ./examples/pandoc-manual.md --toc
-pandoc -d defaults/pdf_weasyprint.yml      -o ./examples/pandoc-manual-weasyprint.pdf    ./examples/pandoc-manual.md --toc
+# pandoc -o examples/spec.epub examples/spec.md --toc
+# pandoc -d defaults/docx.yml -o examples/spec.docx examples/spec.md --toc
+# pandoc -d defaults/html5-github.yml -o examples/spec-github.html examples/spec.md --toc
+# pandoc -d defaults/latex.yml -o examples/spec.tex examples/spec.md --toc
+# # pandoc -d defaults/pdf-eisvogel.yml -o examples/spec-eisvogel.pdf examples/spec.md --toc -M titlepage=true
+# pandoc -d defaults/pdf-pagedjs-cli.yml -o examples/spec-pagedjs-cli.pdf examples/spec.md --toc
+# pandoc -d defaults/pdf-wkhtmltopdf.yml -o examples/spec-wkhtmltopdf.pdf examples/spec.md --toc
+# pandoc -d defaults/pdf-xelatex.yml -o examples/spec-xelatex.pdf examples/spec.md --toc
+# # pandoc -d defaults/pdf-typst.yml           -o examples/spec-typst.pdf         examples/spec.md --toc
+# pandoc -d defaults/pdf-weasyprint.yml -o examples/spec-weasyprint.pdf examples/spec.md --toc
 
 # Test Chinese
-pandoc -d defaults/docx.yml                -o ./examples/README.docx              ./README.md
-pandoc -d defaults/html5_github.yml        -o ./examples/README-github.html       ./README.md
-pandoc -d defaults/latex.yml               -o ./examples/README.tex               ./README.md
-pandoc -d defaults/pdf_ctexart.yml         -o ./examples/README-ctexart.pdf       ./README.md
-pandoc -d defaults/pdf_ctexrep.yml         -o ./examples/README-ctexrep.pdf       ./README.md
-pandoc -d defaults/pdf_eisvogel.yml        -o ./examples/README-eisvogel.pdf      ./README.md
-pandoc -d defaults/pdf_pagedjs-cli.yml     -o ./examples/README-pagedjs-cli.pdf   ./README.md
-pandoc -d defaults/pdf_tufte-handout.yml   -o ./examples/README-tufte-handout.pdf ./README.md
-pandoc -d defaults/pdf_typst.yml           -o ./examples/README-typst.pdf         ./README.md
-pandoc -d defaults/pdf_weasyprint.yml      -o ./examples/README-weasyprint.pdf    ./README.md
-pandoc -d defaults/pdf_wkhtmltopdf.yml     -o ./examples/README-wkhtmltopdf.pdf   ./README.md
-pandoc -d defaults/pdf_xelatex.yml         -o ./examples/README-xelatex.pdf       ./README.md
+pandoc --metadata-file=metadata.yml --metadata-file=metadata1.yml -d defaults/pdf-eisvogel.yml -o examples/README-eisvogel.pdf README.md
+pandoc --metadata-file=metadata.yml --metadata-file=metadata1.yml -d defaults/pdf-tufte-handout.yml -o examples/README-tufte-handout.pdf README.md
+pandoc --metadata-file=metadata.yml --metadata-file=metadata1.yml -d defaults/pdf-xelatex.yml -o examples/README-xelatex.pdf README.md
+pandoc --metadata-file=metadata.yml --metadata-file=metadata2.yml -d defaults/pdf-wkhtmltopdf.yml -o examples/README-wkhtmltopdf.pdf README.md
+pandoc --metadata-file=metadata.yml -d defaults/docx.yml -o examples/README.docx README.md
+pandoc --metadata-file=metadata.yml -d defaults/html5-github.yml -o examples/README-github.html README.md
+pandoc --metadata-file=metadata.yml -d defaults/latex.yml -o examples/README.tex README.md
+pandoc --metadata-file=metadata.yml -d defaults/pdf-ctexart.yml -o examples/README-ctexart.pdf README.md
+pandoc --metadata-file=metadata.yml -d defaults/pdf-ctexrep.yml -o examples/README-ctexrep.pdf README.md
+pandoc --metadata-file=metadata.yml -d defaults/pdf-pagedjs-cli.yml -o examples/README-pagedjs-cli.pdf README.md
+pandoc --metadata-file=metadata.yml -d defaults/pdf-typst.yml -o examples/README-typst.pdf README.md
+pandoc --metadata-file=metadata.yml -d defaults/pdf-weasyprint.yml -o examples/README-weasyprint.pdf README.md
